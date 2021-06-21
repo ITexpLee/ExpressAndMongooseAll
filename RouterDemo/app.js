d8a3c70fb2e57461ba5288eab2d3f6bb780e7d83
Mikel8Lee <connerrobert927@gmail.com>
2021-06-21 15:02:33 +0530

    Created and understood signed and unsigned cookie and how it uses sha256

--- RouterDemo/app.js
+++ RouterDemo/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 //Adding cookie parser
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 //Cookie example
 app.get('/greet', (req, res) => {
@@ -16,6 +16,19 @@ app.get('/setname', (req, res) => {
     res.cookie('name', 'Steve singh');
     res.cookie('animal', 'Lion');
     res.send('Sent you a cookie');
+});
+
+//Signed Cookieess
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', {signed: true});
+    res.send('Singed your fruit cookie');
+});
+
+//Validating signed cookies
+app.get('/verifyfruit', (req, res) => {
+     console.log(req.cookies);
+     console.log(req.signedCookies);
+     res.send(req.signedCookies);
 });
 
 app.listen(3000, () => {
