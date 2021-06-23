401906e3a3cd87ef644da8b4e2c23b1bbc7d9fe3
Mikel8Lee <connerrobert927@gmail.com>
2021-06-24 03:59:46 +0530

    Added authentication to multiple routes and learnt route protection how to logout and destroy session in express

--- BcryptDemo_Authentication/app.js
+++ BcryptDemo_Authentication/app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 //Adding bcrypt 
 const bcrypt = require('bcrypt');
+//Adding session for session cookie and functionality after loggin in
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/authDemo', {
         useNewUrlParser: true,
@@ -29,9 +31,23 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //middlewares 
+app.use(session({
+    secret: 'notagoodsecret',
+    resave: false,
+    saveUninitialized: false
+})); //session middleware
 app.use(express.urlencoded({
     extended: true
 }));
+
+//user created middleware (protect routes)
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 
 app.get('/', (req, res) => {
     res.send('This is the home page')
@@ -56,6 +72,8 @@ app.post('/register', async (req, res) => {
         password: hash
     });
     await user.save();
+    //successfully registered you are logged in
+    req.session.user_id = user._id; 
     res.redirect('/');
 });
 
@@ -72,15 +90,30 @@ app.post('/login', async (req, res) => {
     //bcrypt the password and check for match from fetched user
     const validPassword = await bcrypt.compare(password, user.password);
     if(validPassword) {
-        res.send('Yay welcome');
+        //Now once we are successfully logged in we can store user_id in session cookie
+        req.session.user_id = user._id; 
+        res.redirect('/secret');
     }else{
-        res.send('try again');
+        res.redirect('/login');
     }
 });
 
+//logout route
+app.post('/logout', (req, res) => {
+    //just change it to null
+    req.session.user_id = null;
+    res.redirect('/login');
+});
+
 //secured route
-app.get('/secret', (req, res) => {
-    res.send('You cannot see me until you are signed In');
+app.get('/secret', requireLogin, (req, res) => {
+    //if in session cookie user_id exist we will allow the access (middlware)
+    res.render('secret.ejs');
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    //if in session cookie user_id exist we will allow the access (middlware)
+    res.send('top secret');
 });
 
 app.listen(3000, () => {
